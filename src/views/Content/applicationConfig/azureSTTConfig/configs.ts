b4d69c0903a9a304fd661d6602e62edf590bf290
estarhp <3136551640@qq.com>
2023-09-15 21:43:15 +0800

    feat: SttConfig 页

---
+++ src/views/Content/applicationConfig/azureSTTConfig/configs.ts
@@ -0,0 +1,27 @@
+const propNames = ["application","azure","azure_stt"]
+
+const configs = [
+    {
+        propNames:propNames,
+        title:"Azure 语音服务 API key",
+        label:"api_key"
+    },
+    {
+        propNames:propNames,
+        title:"Azure 语音服务 语音区域",
+        label:"speech_region"
+    },
+    {
+        propNames:propNames,
+        title:"Azure 语音服务 语言",
+        label:"language"
+    },
+    {
+        propNames:propNames,
+        title:"Azure 语音服务 格式",
+        label:"format"
+    },
+
+]
+
+export default configs
