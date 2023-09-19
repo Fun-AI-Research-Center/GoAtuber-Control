bb9eee9e257cb37550fac8f4ccbdef6962add8d5
estarhp <3136551640@qq.com>
2023-09-19 19:39:29 +0800

    feat: 添加数字输入框

--- src/components/TitleInput.tsx
+++ src/components/TitleInput.tsx
@@ -1,5 +1,5 @@
 import type {FC} from "react"
-import {Col, Input, Row} from "antd";
+import {Col, Input, InputNumber, Row} from "antd";
 import {produce} from "immer";
 import {useData} from "../context/DataContext.tsx";
 import React, {useRef} from "react";
@@ -13,13 +13,25 @@ type Props = {
     style?:object,
     frontIcon?:React.ReactNode,
     updateUrl:string,
-    updatePropsName?:string[]
+    updatePropsName?:string[],
+    isNumber?:boolean
 }
-const App : FC<Props> = ({propNames,label,title,style,frontIcon,updateUrl,updatePropsName})=> {
+const App : FC<Props> = ({propNames,label,title,style,frontIcon,updateUrl,updatePropsName,isNumber = false})=> {
     const {data,setData} = useData()
     const {clearTime} = useRef(useUpdateHook(updateUrl)).current
     function handleChange(e:any){
         const newInput = e.target.value;
+        // 使用 immer 更新 proxy_url 属性
+        const newData = produce(data, (draft:any) => {
+            getLastObject(propNames,draft)[label] = newInput;
+        });
+        const newConfig = getLastObject(updatePropsName ? updatePropsName : propNames,newData)
+        clearTime(false,newConfig)
+        setData(newData);
+    }
+
+    function handleNumberChange(number:number | null){
+        const newInput = number;
         // 使用 immer 更新 proxy_url 属性
         const newData = produce(data, (draft:any) => {
             getLastObject(propNames,draft)[label] = newInput;
@@ -37,7 +49,16 @@ const App : FC<Props> = ({propNames,label,title,style,frontIcon,updateUrl,update
                         <h4 style={{margin:0,height:"100%",lineHeight:2}}>{title} {frontIcon}</h4>
                 </Col>
                 <Col span={16}>
-                    <Input defaultValue="mysite" value={getLastObject(propNames,data)[label]} onChange={handleChange}/>
+                    {isNumber ? <InputNumber
+                        value={getLastObject(propNames, data)[label]}
+                        onChange={handleNumberChange}
+                        style={{width:"100%"}}
+                    /> :  <Input
+                        defaultValue="mysite"
+                        value={getLastObject(propNames,data)[label]}
+                        onChange={handleChange}
+                    />}
+
                 </Col>
             </Row>
         </>
