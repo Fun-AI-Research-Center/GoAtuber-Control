c45a7a4dfb81aab974eaa36a5090a0bb8f2ee111
estarhp <3136551640@qq.com>
2023-09-19 19:39:05 +0800

    feat: 开关的接口加入，添加数据更新

--- src/components/TitleSwitch.tsx
+++ src/components/TitleSwitch.tsx
@@ -2,32 +2,31 @@ import type {FC} from "react"
 import {Col, Row, Switch} from "antd";
 import {Config, useData} from "../context/DataContext.tsx";
 import {produce} from "immer";
-import React from "react";
+import React, {useRef} from "react";
+import getLastObject from "../utils/getLastObject.ts";
+import useUpdateHook from "../hooks/useUpdateHook.ts";
 
 type Props = {
     propNames:string[],
     label:string,
     text:string,
-    frontIcon?:React.ReactNode
+    frontIcon?:React.ReactNode,
+    updateUrl:string,
+    updatePropsName?:string[],
 }
 
-const App : FC<Props> = ({propNames,label,text,frontIcon})=>{
+const App : FC<Props> = ({propNames,label,text,frontIcon,updateUrl,updatePropsName})=>{
 
     const {data,setData} = useData()
 
-    function getLastObject(data:any){
-        let LastObject = data
-        propNames.forEach((item) =>{
-            if (LastObject[item]) {
-                LastObject = LastObject[item]
-            }
-        })
-        return LastObject
-    }
+    const {clearTime} = useRef(useUpdateHook(updateUrl)).current
+
     const onChange = (checked: boolean) => {
         const newData = produce(data, (draft:Config) => {
-            getLastObject(draft)[label] = checked
+            getLastObject(propNames,draft)[label] = checked
         });
+        const newConfig = getLastObject(updatePropsName ? updatePropsName : propNames,newData )
+        clearTime(false,newConfig)
         setData(newData);
     };
     return(
@@ -37,7 +36,7 @@ const App : FC<Props> = ({propNames,label,text,frontIcon})=>{
                     <h4 style={{margin:0,height:"100%",lineHeight:1.7}}>{text} {frontIcon}</h4>
                 </Col>
                 <Col >
-                    <Switch checked={getLastObject(data)[label]} onChange={onChange} />
+                    <Switch checked={getLastObject(propNames,data)[label]} onChange={onChange} />
                 </Col>
             </Row>
         </>
